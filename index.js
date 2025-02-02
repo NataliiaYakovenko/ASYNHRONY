e1216d5ad80b4a1a9aa92d67e624df2963b2b7de
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-02 18:58:03 +0200

    Add Асинхронність

--- index.js
+++ index.js
@@ -1,68 +1,21 @@
 'use strict'
 
-//Promise - це об'єкт, який може знаходитись у різних станах:
+//JS - це мова, яка блокується різними діями, наприклад 
+alert('qwerty')
 
-/*promise state (painting,fullfiled, rejected) //якщо promise fullfiled(виконано)
-        painting, - виконується
-        fullfiled, - виконано
-        rejected  - відхилино
-result
+console.log('before timeout');
 
+setTimeout(()=>{console.log('in timeout');},1000)
 
-*/
+console.log('after timeout');
 
-//setInterval(cb,1000)
-//button.addEventListener('click',()=>{});
+function f1(){
+    console.trace();
+    console.log('end');
+}
 
-//fetch - це функція для завантаження даних
-const weatherUrl =
-  "https://apil.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
-
-  fetch(weatherUrl)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(err => console.log('error:',err))
-
-  //Альтернативний варіант
-  // fetch(weatherUrl)
-  // .then(response => response.json())
-  // .then(
-  //   data => console.log(data),
-  //   err => console.log('error:',err)
-  // );
-
-
-
-console.log("next line");
-console.log(weatherUrl);
-
-//console.log(response)
-//-------------------------------------
-//promise
-
-// fetch(weatherUrl)
-// .then(response => response.json())
-// .then(data => console.log(data))
-// .then(data => console.log(data))
-// .then(data => console.log(data))
-// .then(data => console.log(data))
-// .then(data => console.log(data))
-// .then(data => console.log(data))
-// .then(data => console.log(data))
-// .then(data => console.log(data))
-// .catch(err => console.log('error:',err))
-
-//--------------------------------------------
-//calback hell
-
-// setInterval(()=>{
-//   setInterval(()=>{
-//     setInterval(()=>{
-//       setInterval(()=>{
-//         setInterval(()=>{
-//           setInterval(()=>{},1000);
-//         },1000);
-//       },1000);
-//     },1000);
-//   },1000);
-// },1000);
+function f(){
+    console.trace();
+    f1()
+}
+f()
