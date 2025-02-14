e356668472acbf100e7d00f9e8676d58c4c43dc7
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-14 21:11:48 +0200

    Add The weather

--- asynhrony-victoria/index.js
+++ asynhrony-victoria/index.js
@@ -1,5 +1,4 @@
-'use strict'
-
+"use strict";
 
 //відобразити на сторінці поточну температуру з одиницєю виміру
 //відобразити мінусову температуру синім кольором
@@ -21,16 +20,41 @@
 6. Вбудуваи його туди куди потрібно
 */
 const weatherUrl =
-  "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
+  "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
 
-  fetch(weatherUrl)
-  .then(response => response.json())
-  .then(data => generateWeather(data))
-  .catch(err => console.log('error:',err))
+fetch(weatherUrl)
+  .then((response) => response.json())
+  .then((data) => generateWeather(data))
+  .catch((err) => console.log("error:", err));
 
-function generateWeather(data){
-    const currentTemperature = document.createElement('div')
-    currentTemperature.textContent = `${data.current_weather.temperature} ${data.current_weather.units.temperature}`;
-    document.body.append(currentTemperature);
+//відобразити на сторінці поточну температуру з одиницєю виміру
+//відобразити мінусову температуру синім кольором
+// 0 - чорним
+//плюсову температуру до 40 - зеленим
+//температура >= 40 - червоним
+
+function generateWeather(data) {
+  //data.current.temperature_2m = 70 - для тестування температури
+
+  const currentTemperatureEl = document.createElement("div");
+  currentTemperatureEl.textContent = `${data.current.temperature_2m} ${data.hourly_units.temperature_2m}`;
+  currentTemperatureEl.style.color = calcTemperatureColor(data.current.temperature_2m);
+  
+  const currentWindSpeed = document.createElement("div");
+  currentWindSpeed.textContent = `${data.current.wind_speed_10m} ${data.hourly_units.wind_speed_10m}` ;
+
+  document.body.append(currentTemperatureEl);
+  document.body.append(currentWindSpeed)
 }
 
+function calcTemperatureColor(temperature) {
+  if (temperature < 0) {
+    return "blue";
+  } else if (temperature === 0) {
+    return "black";
+  } else if (temperature > 0 && temperature < 40) {
+    return "green";
+  } else {
+    return "red";
+  }
+}
