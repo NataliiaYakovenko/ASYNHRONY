2882df33c3afe555e359c4697f748a7013d3a01f
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-15 08:43:37 +0200

    Add Промісифікувати setTimeout

--- asynhrony-victoria/index.js
+++ asynhrony-victoria/index.js
@@ -40,3 +40,27 @@ const schredingerCat = new Promise(executor);
 schredingerCat
 .then(data=>console.log(data))
 .catch(error=>console.log(error));
+//-------------------------------------------------------
+
+//Промісифікувати setTimeout
+//setTimeout(cb,1000)
+//delay(1000).then(cb)
+
+function delay(ms){
+  function executor(resolve,reject){
+      if(typeof ms !== 'number'){
+         reject (new TypeError('milliseconds must be a number'))
+      }
+      if(ms < 0 || !Number.isInteger(ms)){
+        reject (new RangeError('milliseconds must be more 0 and must be integer'))
+      }
+       setTimeout(resolve,ms)
+     }
+     
+  return new Promise(executor)
+
+}
+//setTimeout(()=>console.log('action is over'),1000)
+delay(2000)
+.then(()=>console.log('action is over'))
+.catch(error => console.log(error))
