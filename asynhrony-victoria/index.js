19bdd0078aa76cb022b48c5abda09f78764033b1
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-15 08:04:01 +0200

    Add Кіт Шредингера

--- asynhrony-victoria/index.js
+++ asynhrony-victoria/index.js
@@ -5,14 +5,38 @@
 // statuse fulfilled (виконано) => then
 // statuse reject (відхилено) => catch
 
-function promiseCallback(resolve,reject){
- //виконуються кісь дії
- //resolve('success result')
- reject(new Error('something wrong'))
+function promiseCallback(resolve, reject) {
+  //виконуються кісь дії
+  //resolve('success result')
+  reject(new Error("something wrong"));
 }
 
-const promise = new Promise(promiseCallback)
+const promise = new Promise(promiseCallback);
 
-promise
+promise.then((data) => console.log(data)).catch((error) => console.log(error));
+//----------------------------------------------------------------------
+
+// Promise.resolve([{user:'Nataliia'}])
+// .then(data=>console.log(data))
+
+// Promise.reject()
+// .catch()
+
+//синхроний код виконується до promise
+console.log("end of synchronous code");
+//---------------------------------------------------------------------
+//Задача. Кіт Шредингера
+
+function executor(resolve, reject) {
+  if (Math.random() < 0.5) {
+    resolve('the cat is alive')
+  } else {
+    reject(new Error ('the cat is dead'))
+  }
+}
+
+const schredingerCat = new Promise(executor);
+
+schredingerCat
 .then(data=>console.log(data))
-.catch(error=>console.log(error))
+.catch(error=>console.log(error));
